d36263f044482778d1807f9c204bcfe51e137118
Alexandr Savinov <savinov@conceptoriented.org>
2020-05-17 14:06:19 +0200

    merge source input files into one output file with all source columns

--- scripts/merge_data.py
+++ scripts/merge_data.py
@@ -14,32 +14,33 @@ from trade.utils import *
 from trade.feature_generation import *
 
 """
-Create one output file from input files of different types: depth, futures, klines.
+Create one output file from input files of different types: klines, futures, depth.
 Depth data can be provided in several files. 
 Also, depth timestamps correspond to end of 1m interval and hence they will be changed to kline convention.
 Futures and klines are in single files and their timestamp is start of 1m interval.
-Output is one file with all the columns from input files.
-Futures columns are prefixed and suffixed (optionally) in order to distinguish them from klines data.
+Future column names are same as in klines, and hence they will be prefixed in the output.
+Output file has continuous index by removing possible gaps in input files.
 """
 
 
 symbol = "BTCUSDT"  # BTCUSDT ETHBTC IOTAUSDT
 
-depth_file_names = [
-    r"C:\DATA2\BITCOIN\GENERATED\depth-BTCUSDT-batch1.csv",
-    r"C:\DATA2\BITCOIN\GENERATED\depth-BTCUSDT-batch2.csv",
-    r"C:\DATA2\BITCOIN\GENERATED\depth-BTCUSDT-batch3.csv",
-    r"C:\DATA2\BITCOIN\GENERATED\depth-BTCUSDT-batch4.csv",
-    r"C:\DATA2\BITCOIN\GENERATED\depth-BTCUSDT-batch5.csv",
-]
+kline_file_name = r"C:\DATA2\BITCOIN\DOWNLOADED\BTCUSDT-1m-klines.csv"
 
 futur_file_name = r"C:\DATA2\BITCOIN\DOWNLOADED\BTCUSDT-1m-futurs.csv"
 
-kline_file_name = r"C:\DATA2\BITCOIN\DOWNLOADED\BTCUSDT-1m-klines.csv"
+depth_file_names = [  # Leave empty to skip
+    #r"C:\DATA2\BITCOIN\GENERATED\depth-BTCUSDT-batch1.csv",
+    #r"C:\DATA2\BITCOIN\GENERATED\depth-BTCUSDT-batch2.csv",
+    #r"C:\DATA2\BITCOIN\GENERATED\depth-BTCUSDT-batch3.csv",
+    #r"C:\DATA2\BITCOIN\GENERATED\depth-BTCUSDT-batch4.csv",
+    #r"C:\DATA2\BITCOIN\GENERATED\depth-BTCUSDT-batch5.csv",
+]
 
-out_file_name = r"aaa.csv"
+out_file_name = r"BTCUSDT-1m.csv"
 
 futur_column_prefix = "f_"
+range_type = "kline"  # Selector: kline, futur, depth, merged (common range)
 
 #
 # Historic data
@@ -53,6 +54,32 @@ def get_symbol_files(symbol):
     file_pattern = f"*{symbol}*.txt"
     paths = Path(in_path_name).rglob(file_pattern)
     return list(paths)
+
+def load_futur_files():
+    """Return a data frame with future features."""
+
+    df = pd.read_csv(futur_file_name, parse_dates=['timestamp'])
+    start = df["timestamp"].iloc[0]
+    end = df["timestamp"].iloc[-1]
+
+    df = df.set_index("timestamp")
+
+    print(f"Loaded futur file with {len(df)} records in total. Range: ({start}, {end})")
+
+    return df, start, end
+
+def load_kline_files():
+    """Return a data frame with kline features."""
+
+    df = pd.read_csv(kline_file_name, parse_dates=['timestamp'])
+    start = df["timestamp"].iloc[0]
+    end = df["timestamp"].iloc[-1]
+
+    df = df.set_index("timestamp")
+
+    print(f"Loaded kline file with {len(df)} records in total. Range: ({start}, {end})")
+
+    return df, start, end
 
 def load_depth_files():
     """Return a list of data frames with depth features."""
@@ -82,47 +109,42 @@ def load_depth_files():
 
     return dfs, start, end
 
-def load_futur_files():
-    """Return a data frame with future features."""
-
-    df = pd.read_csv(futur_file_name, parse_dates=['timestamp'])
-    start = df["timestamp"].iloc[0]
-    end = df["timestamp"].iloc[-1]
-
-    df = df.set_index("timestamp")
-
-    print(f"Loaded futur file with {len(df)} records in total. Range: ({start}, {end})")
-
-    return df, start, end
-
-def load_kline_files():
-    """Return a data frame with kline features."""
-
-    df = pd.read_csv(kline_file_name, parse_dates=['timestamp'])
-    start = df["timestamp"].iloc[0]
-    end = df["timestamp"].iloc[-1]
-
-    df = df.set_index("timestamp")
-
-    print(f"Loaded kline file with {len(df)} records in total. Range: ({start}, {end})")
-
-    return df, start, end
-
 def main(args=None):
 
     start_dt = datetime.now()
     print(f"Start processing...")
 
-    d_dfs, d_start, d_end = load_depth_files()
-    f_df, f_start, f_end = load_futur_files()
     k_df, k_start, k_end = load_kline_files()
+    f_df, f_start, f_end = load_futur_files()
+    if depth_file_names:
+        d_dfs, d_start, d_end = load_depth_files()
+    else:
+        d_dfs = []
+        d_start = 0
+        d_end = 0
 
-    # Common start
-    start = np.max([d_start, f_start, k_start])
-    # Common end
-    end = np.min([d_end, f_end, k_end])
+    #
+    # Determine range
+    #
+    if range_type.startswith("kline"):
+        start = k_start
+        end = k_end
+    elif range_type.startswith("futur"):
+        start = f_start
+        end = f_end
+    elif range_type.startswith("depth"):
+        start = d_start
+        end = d_end
+    elif range_type.startswith("merge"):
+        start = np.max([d_start, f_start, k_start])
+        end = np.min([d_end, f_end, k_end])
+    else:
+        print(f"Unknown parameter value. Exit.")
+        exit()
 
-    # Create common 1m index and empty data frame
+    #
+    # Create 1m common (main) index and empty data frame
+    #
     index = pd.date_range(start, end, freq="T")
     df_out = pd.DataFrame(index=index)
     df_out.index.name = "timestamp"
@@ -131,6 +153,13 @@ def main(args=None):
     # Attach all necessary columns to the common data frame
     #
 
+    # Attach kline data frame
+    df_out = df_out.join(k_df)
+
+    # Attach futur data frame by also renaming columns
+    f_df = f_df.rename(lambda x: futur_column_prefix + x if x != "timestamp" else x, axis='columns')
+    df_out = df_out.join(f_df)
+
     # Attach several depth data frames using the same columns
     for i, df in enumerate(d_dfs):
         if i == 0:
@@ -138,15 +167,10 @@ def main(args=None):
         else:
             df_out.update(df)
 
-    # Attach futur data frame by also renaming columns
-    f_df = f_df.rename(lambda x: futur_column_prefix + x if x != "timestamp" else x, axis='columns')
-    df_out = df_out.join(f_df)
-
-    # Attach kline data frame
-    df_out = df_out.join(k_df)
-
+    #
     # Store file with features
-    df_out.to_csv(out_file_name, index=True, float_format="%.6f")
+    #
+    df_out.to_csv(out_file_name, index=True)  # float_format="%.6f"
     print(f"Stored output merged file with {len(df_out)} records. Range: ({start}, {end})")
 
     elapsed = datetime.now() - start_dt
