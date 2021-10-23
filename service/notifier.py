45022c2647dbaca65293549a463f5cc0846a0b05
Alexandr Savinov <savinov@conceptoriented.org>
2021-10-23 21:51:55 +0200

    notification options

--- service/notifier.py
+++ service/notifier.py
@@ -10,12 +10,42 @@ from common.utils import *
 async def notify_telegram():
     status = App.status
     signal = App.signal
+    notification_threshold = App.config["signaler"]["notification_threshold"]
+    symbol = App.config["symbol"]
+
     signal_side = signal.get("side")
     score = signal.get('score')
 
-    sign = "+++>>>" if score > 0 else "---<<<"
+    if -notification_threshold < score < notification_threshold:
+        return
 
-    message = f"{sign} {score:+.2f}. PRICE: {int(signal.get('close_price'))}. STATUS: {status}"
+    if score < -0.5:
+        sign = "<<<<<"
+    elif score < -0.4:
+        sign = "<<<<"
+    elif score < -0.3:
+        sign = "<<<"
+    elif score < -0.2:
+        sign = "<<"
+    elif score < -0.1:
+        sign = "<"
+    elif score < +0.1:
+        sign = ""
+    elif score < +0.2:
+        sign = ">"
+    elif score < +0.3:
+        sign = ">>"
+    elif score < +0.4:
+        sign = ">>>"
+    elif score < +0.5:
+        sign = ">>>>"
+    else:
+        sign = ">>>>>"
+
+    message = f"{sign} {score:+.2f}. {symbol} {int(signal.get('close_price')):,}"
+
+    if signal.get('side') in ["BUY", "SELL"]:
+        message = f"*{message}. SIGNAL: {signal.get('side')}*"
 
     bot_token = App.config["telegram_bot_token"]
     chat_id = App.config["telegram_chat_id"]
