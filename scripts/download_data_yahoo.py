3002c8833f895d5933cf5d1c884436232ae75516
Alexandr Savinov <savinov@conceptoriented.org>
2022-07-16 10:09:56 +0200

    merge data daily raster and improvements

--- scripts/download_data_yahoo.py
+++ scripts/download_data_yahoo.py
@@ -17,6 +17,8 @@ def main(config_file):
     """
     load_config(config_file)
 
+    time_column = App.config["time_column"]
+
     data_path = Path(App.config["data_folder"])
     if not data_path.is_dir():
         print(f"Data folder does not exist: {data_path}")
@@ -26,24 +28,24 @@ def main(config_file):
     today = date.today()
 
     data_sources = App.config["data_sources"]
-    for data_source in data_sources:
+    for ds in data_sources:
         # Assumption: folder name is equal to the symbol name we want to download
-        quote = data_source.get("folder")
+        quote = ds.get("folder")
         if not quote:
             print(f"ERROR. Folder is not specified.")
             continue
 
         # If file name is not specified then use symbol name as file name
-        file = data_source.get("file", quote)
+        file = ds.get("file", quote)
         if not file:
             file = quote
 
         print(f"Start downloading '{quote}' ...")
 
-        in_file_path = data_path / quote / file
+        in_file_path = (data_path / quote / file).with_suffix(".csv")
 
-        if in_file_path.with_suffix(".csv").is_file():
-            df = pd.read_csv(in_file_path.with_suffix(".csv"), parse_dates=["date"])
+        if in_file_path.is_file():
+            df = pd.read_csv(in_file_path, parse_dates=["date"])
             #df['Date'] = pd.to_datetime(df['Date'])  # "2022-06-07" iso format
             df['date'] = df['date'].dt.date
             last_date = df.iloc[-1]['date']
@@ -54,11 +56,11 @@ def main(config_file):
             new_df = new_df.reset_index()
             new_df['Date'] = pd.to_datetime(new_df['Date']).dt.date
             del new_df['Close']
-            new_df.rename({'Adj Close': 'Close'}, axis=1, inplace=True)
+            new_df.rename({'Adj Close': 'Close', 'Date': time_column}, axis=1, inplace=True)
             new_df.columns = new_df.columns.str.lower()
 
             df = pd.concat([df, new_df])
-            df = df.drop_duplicates(subset=["date"], keep="last")
+            df = df.drop_duplicates(subset=[time_column], keep="last")
 
         else:
             print(f"File not found. Full fetch...")
@@ -69,15 +71,15 @@ def main(config_file):
             df = df.reset_index()
             df['Date'] = pd.to_datetime(df['Date']).dt.date
             del df['Close']
-            df.rename({'Adj Close': 'Close'}, axis=1, inplace=True)
+            df.rename({'Adj Close': 'Close', 'Date': time_column}, axis=1, inplace=True)
             df.columns = df.columns.str.lower()
 
             print(f"Full fetch finished.")
 
-        df = df.sort_values(by="date")
+        df = df.sort_values(by=time_column)
 
         in_file_path.parent.mkdir(parents=True, exist_ok=True)
-        df.to_csv(in_file_path.with_suffix(".csv"), index=False)
+        df.to_csv(in_file_path, index=False)
         print(f"Stored in '{in_file_path}'")
 
     elapsed = datetime.now() - now
--- scripts/merge_data.py
+++ scripts/merge_data.py
@@ -1,38 +1,20 @@
-import pandas as pd
-import math
-#import os.path
+from datetime import timedelta, datetime, time
 from pathlib import Path
-import json
-import time
-from datetime import timedelta, datetime
-from dateutil import parser
-from tqdm import tqdm_notebook #(Optional, used for progress-bars)
-import click
 
+import pandas as pd
 import numpy as np
 
-from common.utils import *
+import click
+
 from service.App import *
-from common.feature_generation import *
 
 """
-Create one output file from many input files:
-- symbols like BTC or ETH
-- different data types like klines, futures, depth (depth can be in several files)
-
-Align the timestamps and create a uniform time axis without gaps.
-
-Prefix column names with the corresponding origin modifiers so that each column name stores its origin.
-
-IMPLEMENTATION.
-What we need to know:
-- data folder with all files, symbol list to get subfolders, data types for each symbol to find its individual file, prefix to be used for this file
-- once files are loaded, create raster index, and merge all loaded data to this index by also providing column prefixes.
-- what to do with empty (initial or trailing)? Automatically detect maximum common timestamp.
-
-list of:
-<symbol>-<data source> - prefix
-
+This script is intended for creating one output file from multiple input data files. 
+It is needed when we want to use additional data source in order to predict the main parameter.
+For example, in order to predict BTC price, we might want to add ETH prices. 
+This script solves the following problems:
+- Input files might have the same column names (e.g., open, high, low, close) and therefore it adds prefixes to the columns of the output file
+- Input data may have gaps and therefore the script generates a regular time raster for the output file. The granularity of the time raster is determined by the parameter
 """
 
 
@@ -44,12 +26,9 @@ depth_file_names = [  # Leave empty to skip
     #r"C:\DATA2\BITCOIN\GENERATED\depth-BTCUSDT-batch5.csv",
 ]
 
-futur_column_prefix = "f_"
-range_type = "kline"  # Selector: kline, futur, depth, merged (common range)
-
 
 #
-# Historic data
+# Readers from inputs files (DEPRECATED)
 #
 
 def load_futur_files(futur_file_path):
@@ -108,72 +87,97 @@ def load_depth_files():
 
     return dfs, start, end
 
+#
+# Merger
+#
+
 
 @click.command()
 @click.option('--config_file', '-c', type=click.Path(), default='', help='Configuration file name')
 def main(config_file):
     load_config(config_file)
 
-    freq = "1m"
-    symbol = App.config["symbol"]
-    data_path = Path(App.config["data_folder"])
+    time_column = App.config["time_column"]
+
     data_sources = App.config.get("data_sources", [])
     if not data_sources:
-        data_sources = [{"folder": symbol, "file": "klines", "column_prefix": ""}]
+        print(f"ERROR: Data sources are not defined. Nothing to merge.")
+        #data_sources = [{"folder": symbol, "file": "klines", "column_prefix": ""}]
 
-    config_file_modifier = App.config.get("config_file_modifier")
-    config_file_modifier = ("-" + config_file_modifier) if config_file_modifier else ""
+    #config_file_modifier = App.config.get("config_file_modifier")
+    #config_file_modifier = ("-" + config_file_modifier) if config_file_modifier else ""
 
-    start_dt = datetime.now()
+    now = datetime.now()
 
-    # Read data from files
+    # Read data from input files
+    data_path = Path(App.config["data_folder"])
     for ds in data_sources:
         # What is want is for each source, load file into df, determine its properties (columns, start, end etc.), and then merge all these dfs
-        file_path = (data_path / ds.get("folder") / ds.get("file")).with_suffix(".csv")
+
+        quote = ds.get("folder")
+        if not quote:
+            print(f"ERROR. Folder is not specified.")
+            continue
+
+        # If file name is not specified then use symbol name as file name
+        file = ds.get("file", quote)
+        if not file:
+            file = quote
+
+        file_path = (data_path / quote / file).with_suffix(".csv")
         if not file_path.is_file():
             print(f"Data file does not exist: {file_path}")
             return
 
         print(f"Reading data file: {file_path}")
-        df = pd.read_csv(file_path, parse_dates=['timestamp'])
+        df = pd.read_csv(file_path, parse_dates=[time_column])
         print(f"Loaded file with {len(df)} records.")
+
         ds["df"] = df
 
     # Merge in one df with prefixes and common regular time index
     df_out = merge_data_frames(data_sources)
-    range_start = df_out.index[0]
-    range_end = df_out.index[-1]
 
     #
     # Store file with features
     #
-    out_file_suffix = App.config.get("merge_file_modifier")
-
-    out_file_name = f"{out_file_suffix}{config_file_modifier}.csv"
-    out_path = data_path / symbol / out_file_name
+    out_path = data_path / App.config["symbol"] / App.config.get("merge_file_modifier")
 
     print(f"Storing output file...")
-    df_out.to_csv(out_path, index=True)  # float_format="%.6f"
+    df_out.to_csv(out_path.with_suffix(".csv"), index=True)  # float_format="%.6f"
+    range_start = df_out.index[0]
+    range_end = df_out.index[-1]
     print(f"Stored output merged file with {len(df_out)} records. Range: ({range_start}, {range_end})")
 
-    elapsed = datetime.now() - start_dt
-    print(f"Finished processing in {int(elapsed.total_seconds())} seconds.")
-    print(f"Output file location: {out_path}")
+    elapsed = datetime.now() - now
+    print(f"Finished merging data in {str(elapsed).split('.')[0]}")
+    print(f"Output file location: {out_path.with_suffix('.csv')}")
 
 
 def merge_data_frames(data_sources: list):
+
+    time_column = App.config["time_column"]
+    freq = App.config["freq"]
+
     for ds in data_sources:
         df = ds.get("df")
 
-        if "timestamp" in df.columns:
-            df = df.set_index("timestamp")
+        if time_column in df.columns:
+            df = df.set_index(time_column)
+        else:
+            print(f"ERROR: Timestamp column is absent.")
+            return
 
+        # Add prefix if not already there
         if ds['column_prefix']:
-            df = df.add_prefix(ds['column_prefix']+"_")
-        #df.columns = [f"{ds['column_prefix']}_{col}" for col in df.columns]
+            #df = df.add_prefix(ds['column_prefix']+"_")
+            df.columns = [
+                ds['column_prefix']+"_"+col if not col.startswith(ds['column_prefix']+"_") else col
+                for col in df.columns
+            ]
 
-        ds["start"] = df.index[0]
-        ds["end"] = df.index[-1]
+        ds["start"] = df.first_valid_index()  # df.index[0]
+        ds["end"] = df.last_valid_index()  # df.index[-1]
 
         ds["df"] = df
 
@@ -183,9 +187,17 @@ def merge_data_frames(data_sources: list):
     range_start = min([ds["start"] for ds in data_sources])
     range_end = min([ds["end"] for ds in data_sources])
 
-    index = pd.date_range(range_start, range_end, freq="T")
+    # Regular time raster according to the parameter
+    if freq == "1m":
+        index = pd.date_range(range_start, range_end, freq="T")
+    elif freq == "1d":
+        index = pd.date_range(range_start, range_end, freq="B")  # D - daily, B - business days (no weekends)
+    else:
+        print(f"ERROR: Frequency parameter 'freq' is unknown or not specified: {freq}")
+        return
+
     df_out = pd.DataFrame(index=index)
-    df_out.index.name = "timestamp"
+    df_out.index.name = time_column
 
     for ds in data_sources:
         # Note that timestamps must have the same semantics, for example, start of kline (and not end of kline)
