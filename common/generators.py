b4b75662b66c24f0647d7a5e32f3043afa87cad7
Alexandr Savinov <savinov@conceptoriented.org>
2023-12-10 11:51:26 +0100

    added a dispatcher for generators

---
+++ common/generators.py
@@ -0,0 +1,98 @@
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+
+from common.gen_features import *
+from common.gen_labels_highlow import generate_labels_highlow, generate_labels_highlow2
+from common.gen_labels_topbot import generate_labels_topbot, generate_labels_topbot2
+from common.gen_signals import (
+    generate_smoothen_scores, generate_combine_scores,
+    generate_threshold_rule, generate_threshold_rule2
+)
+
+
+def generate_feature_set(df: pd.DataFrame, fs: dict, last_rows: int) -> Tuple[pd.DataFrame, list]:
+    """
+    Apply the specified resolved feature generator to the input data set.
+    """
+
+    #
+    # Select columns from the data set to be processed by the feature generator
+    #
+    cp = fs.get("column_prefix")
+    if cp:
+        cp = cp + "_"
+        f_cols = [col for col in df if col.startswith(cp)]
+        f_df = df[f_cols]  # Alternatively: f_df = df.loc[:, df.columns.str.startswith(cf)]
+        # Remove prefix because feature generators are generic (a prefix will be then added to derived features before adding them back to the main frame)
+        f_df = f_df.rename(columns=lambda x: x[len(cp):] if x.startswith(cp) else x)  # Alternatively: f_df.columns = f_df.columns.str.replace(cp, "")
+    else:
+        f_df = df[df.columns.to_list()]  # We want to have a different data frame object to add derived featuers and then join them back to the main frame with prefix
+
+    #
+    # Resolve and apply feature generator functions from the configuration
+    #
+    generator = fs.get("generator")
+    gen_config = fs.get('config', {})
+    if generator == "itblib":
+        features = generate_features_itblib(f_df, gen_config, last_rows=last_rows)
+    elif generator == "depth":
+        features = generate_features_depth(f_df)
+    elif generator == "tsfresh":
+        features = generate_features_tsfresh(f_df, gen_config, last_rows=last_rows)
+    elif generator == "talib":
+        features = generate_features_talib(f_df, gen_config, last_rows=last_rows)
+    elif generator == "itbstats":
+        features = generate_features_itbstats(f_df, gen_config, last_rows=last_rows)
+
+    # Labels
+    elif generator == "highlow":
+        horizon = gen_config.get("horizon")
+
+        # Binary labels whether max has exceeded a threshold or not
+        print(f"Generating 'highlow' labels with horizon {horizon}...")
+        features = generate_labels_highlow(f_df, horizon=horizon)
+
+        print(f"Finished generating 'highlow' labels. {len(features)} labels generated.")
+    elif generator == "highlow2":
+        print(f"Generating 'highlow2' labels...")
+        f_df, features = generate_labels_highlow2(f_df, gen_config)
+        print(f"Finished generating 'highlow2' labels. {len(features)} labels generated.")
+    elif generator == "topbot":
+        column_name = gen_config.get("columns", "close")
+
+        top_level_fracs = [0.01, 0.02, 0.03, 0.04, 0.05]
+        bot_level_fracs = [-x for x in top_level_fracs]
+
+        f_df, features = generate_labels_topbot(f_df, column_name, top_level_fracs, bot_level_fracs)
+    elif generator == "topbot2":
+        f_df, features = generate_labels_topbot2(f_df, gen_config)
+
+    # Signals
+    elif generator == "smoothen":
+        f_df, features = generate_smoothen_scores(f_df, gen_config)
+    elif generator == "combine":
+        f_df, features = generate_combine_scores(f_df, gen_config)
+    elif generator == "threshold_rule":
+        f_df, features = generate_threshold_rule(f_df, gen_config)
+    elif generator == "threshold_rule2":
+        f_df, features = generate_threshold_rule2(f_df, gen_config)
+
+    else:
+        print(f"Unknown feature generator {generator}")
+        return
+
+    #
+    # Add generated features to the main data frame with all other columns and features
+    #
+    f_df = f_df[features]
+    fp = fs.get("feature_prefix")
+    if fp:
+        f_df = f_df.add_prefix(fp + "_")
+
+    new_features = f_df.columns.to_list()
+
+    df = df.join(f_df)  # Attach all derived features to the main frame
+
+    return df, new_features
